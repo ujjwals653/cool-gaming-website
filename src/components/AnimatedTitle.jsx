1ddea1a0720bd4de3645b6fbf238e94d24e9bde1
ujjwals653 <ujjwals653@gmail.com>
2025-08-19 20:20:13 +0530

    About Section done with Animated text

---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,53 @@
+import { useGSAP } from "@gsap/react";
+import clsx from "clsx";
+import { useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+
+function AnimatedTitle({ title, containerClass }) {
+  const containerRef = useRef(null);
+
+  useGSAP(
+    () => {
+      const titleAnimation = gsap.timeline({
+        scrollTrigger: {
+          trigger: containerRef.current,
+          start: "100 bottom",
+          end: "center bottom",
+          toggleActions: "play none none reverse",
+        },
+      });
+
+      titleAnimation.to(".animated-word", {
+        opacity: 1,
+        transform: "translate3d(0, 0, 0) rotateY(0deg) rotateX(0deg)",
+        ease: "power2.inOut",
+        stagger: 0.02,
+      });
+    },
+    { scope: containerRef }
+  );
+
+  return (
+    <div ref={containerRef} className={clsx("animated-title", containerClass)}>
+      {title.split("<br />").map((line, index) => (
+        <div
+          key={index}
+          className="flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3"
+        >
+          {line.split(" ").map((word, idx) => (
+            <span
+              key={idx}
+              className="animated-word"
+              dangerouslySetInnerHTML={{ __html: word }}
+            />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default AnimatedTitle;
